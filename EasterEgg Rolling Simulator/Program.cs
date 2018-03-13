2a50a55b9f48120fb77a427e0df3df516ba792b7
Casper Andersen <zbccaand49@zbc.dk>
2018-03-13 13:05:27 +0100

    Add project files.

---
+++ EasterEgg Rolling Simulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasterEgg_Rolling_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
